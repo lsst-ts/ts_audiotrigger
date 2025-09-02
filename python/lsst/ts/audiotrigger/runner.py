b694dcbbe8d2a5257cc6aada2ef32d6de78487a2
Eric Coughlin <ecoughlin@lsst.org>
2025-09-02 07:29:49 -0500

    Fix lal_log_port argument in cli.

--- python/lsst/ts/audiotrigger/runner.py
+++ python/lsst/ts/audiotrigger/runner.py
@@ -63,6 +63,7 @@ class Runner:
         self.log = log
         self.disable_microphone = disable_microphone
         self.simulation_mode = simulation_mode
+        self.lal_log_port = lal_log_port
         self.laser_alignment = None
         self.serial_scanner = None
         self.run_task = utils.make_done_future()
@@ -81,6 +82,7 @@ class Runner:
             log=self.log,
             simulation_mode=self.simulation_mode,
             disable_microphone=self.disable_microphone,
+            port=self.lal_log_port,
         )
         self.serial_scanner = SerialTemperatureScanner(
             log=self.log, simulation_mode=self.simulation_mode
